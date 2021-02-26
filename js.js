69a8f4e4158f01534f2fe6eaafbf9608e80b297a
Андрей <example@gmail.com>
2021-02-26 19:31:35 +0400

    first commit

---
+++ js.js
@@ -0,0 +1,50 @@
+let num = 50;
+
+if (num < 49) {
+    console.log("Не верно!")
+} else if (num > 100) {
+    console.log("Много!")
+} else {
+    console.log("Верно!")
+};
+
+(num == 50) ? console.log("Верно!") : console.log("Не верно!");
+
+
+switch (num) {
+    case num < 49:
+        console.log('Не верно!');
+        break;
+    case num > 100:
+        console.log("Много!");
+        break;
+    case num > 80:
+        console.log("Всё ещё много!");
+        break;
+    case 50:
+        console.log('Верно!');
+        break;
+    default:
+        console.log("Что-то пошло не так!");
+        break;
+}
+
+let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue
+    }
+    console.log(i)
+}
+
